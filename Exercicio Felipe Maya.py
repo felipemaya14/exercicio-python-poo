abbbdd65f80fd4f66bcddbb8e61936d43aa0dedc
Felipe M. <felipemaya14@gmail.com>
2025-12-19 20:26:27 -0300

    Add banking functionality and refactor inventory code
    Refactor inventory management code and add banking features.

--- Exercicio Felipe Maya.py
+++ Exercicio Felipe Maya.py
@@ -1,47 +1,24 @@
-#1. Dicionário de estoque:
-estoque_loja = {
-    "monitor": 15,
-    "teclado": 25,
-    "mouse": 30
-}
-#2. Dicionário de preços:
-
-precos = {
-    "monitor": 850.50,
-    "teclado": 120.00,
-    "mouse": 85.00
-}
-
-#3. Entrada de produto e quantidade:
-
-produto = input("Digite o produto que deseja comprar: ").lower() # Entrada do protudo que o cliente deseja comprar já em minusculo.
-
-if produto in estoque_loja:
-    print(f"Quantidade disponível: {estoque_loja[produto]}") # Verifica se o produto existe no estoque.
-else:
-    print("Produto indisponível.") # Caso o produto nao exista no estoque.
-quantidade = int(input("Quantidade desejada: ")) # Entrada da quantidade que o cliente deseja comprar.
-
-if quantidade <= estoque_loja[produto]:
-    estoque_loja[produto] -= quantidade # Atualiza o estoque subtraindo a quantidade vendida.
-    preco = precos[produto] * quantidade # Calcula o valor total da venda.
-    print(f"Compra realizada: {quantidade} unidades de {produto}. Total: R${preco:.2f}")
-else:
-    print("Quantidade indisponível.") # Caso a quantidade solicitada seja maior que a quantidade em estoque.
+class ContaBancaria:
+    def __init__(self, titular):
+        self.titular = titular
+        self.saldo = 0
+        
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"Deposito de R${valor} realizado. Saldo atual: R${self.saldo}")
     
-#4. Calculo do imposto:
-def calcular_imposto(preco):
-    imposto = preco * 0.15 # Calcula o imposto de 15% sobre o preco.
-    return imposto
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+            print(f"Saque de R${valor} realizado. Saldo atual: R${self.saldo}")
+        else:
+            print("Saldo insuficiente")
+    
+# Teste
+conta = ContaBancaria("Felipe")
 
-preco = precos[produto] * quantidade # Calcula o preco total da venda.
-imposto = calcular_imposto(preco)
-print(f"Valor do imposto: RS: {imposto:.2f}")
+valor_deposito = float(input("Digite o valor para depósito: R$ "))
+conta.depositar(valor_deposito)
 
-#5. Calculo do lucro:
-
-for produto, quantidade in estoque_loja.items(): # Exibe o nome e o estoque de cada produto.
-    print(f"Estoque atual de {produto.upper()}: {quantidade} unidades.")
-
-lucro_total = preco - imposto # Calcula o lucro total.
-print(f"Lucro total: R${lucro_total:.2f}")
+valor_saque = float(input("Digite o valor para saque: R$ "))
+conta.sacar(valor_saque)
